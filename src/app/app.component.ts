5e8ea99e5705629e6d916a4bda8ba00948ca2b3f
matheus <matheus.massari@protonmail.com>
2022-05-11 10:50:14 -0300

    todo component complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todos';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+    title: string = 'Lista To-Do';
+    todos: Todo[] = [];
+    inputTodo: string = '';
+
+    ngOnInit(): void {
+        this.todos = [
+            {
+                content: 'Atividade x',
+                completed: false,
+            },
+            {
+                content: 'Atividade y',
+                completed: false,
+            },
+            {
+                content: 'Atividade z',
+                completed: false,
+            },
+        ];
+    }
+
+
+    deleteTodo(id: number) {
+        this.todos = this.todos.filter((v, i) => i !== id);
+    }
+
+    addTodo() {
+        this.todos.push({
+            content: this.inputTodo,
+            completed: false,
+        });
+
+        this.inputTodo = "";
+    }
 }
